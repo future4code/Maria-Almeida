8eb607644c2d1755f8079001f6be0d0944d8df09
Maria <m.franchidealmeida@gmail.com>
2021-04-20 22:37:54 -0300

    blog

---
+++ quinzena4/objetos/blog/blog-boilerplate/index.js
@@ -0,0 +1,18 @@
+function postagem() {
+    
+    const tituloPost= document.getElementById ("titulo-post").value, 
+    const autorPost= document.getElementById ("autor-post").value, 
+    const conteudoPost= document.getElementById ("conteudo-post").value 
+}
+
+console.log(tituloPost)
+console.log(autorPost)
+console.log(conteudoPost)
+
+let user = {
+    titulo: document.getElementById ("titulo-post").value = '',
+    autor: document.getElementById("autor-post").value = '',
+    conteudo: document.getElementById ("conteudo-post").value = '',
+}
+
+
