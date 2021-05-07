9ca1b11424c5efdfb0c4487c355a6e601edf0535
Maria <m.franchidealmeida@gmail.com>
2021-05-06 23:28:55 -0300

    renderizacao

--- quinzena6/insta4/src/App.js
+++ quinzena6/insta4/src/App.js
@@ -10,29 +10,24 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-<Post
-          nomeUsuario={'luara'}
-          fotoUsuario={'https://cdn.pixabay.com/photo/2019/01/25/11/18/girl-3954232_960_720.jpg'}
-          fotoPost={'https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg'}
-        />
-
-<Post
-          nomeUsuario={'olga'}
-          fotoUsuario={'https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237_960_720.jpg'}
-          fotoPost={'https://cdn.pixabay.com/photo/2017/11/11/21/55/girl-2940655_960_720.jpg'}
-        />
-      </MainContainer>
-    );
+  state = {
+    post: [
+  {
+          nomeUsuario: "paulinha",
+          fotoUsuario: "https://picsum.photos/50/50",
+          fotoPost: "https://picsum.photos/200/150",
+  },
+  {
+          nomeUsuario: "luara",
+          fotoUsuario: "https://cdn.pixabay.com/photo/2019/01/25/11/18/girl-3954232_960_720.jpg"
+          fotoPost: "https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg"
+  },
+  {
+          nomeUsuario: "olga",
+          fotoUsuario: "https://cdn.pixabay.com/photo/2014/09/23/06/04/brothers-457237_960_720.jpg",
+          fotoPost: "https://cdn.pixabay.com/photo/2017/11/11/21/55/girl-2940655_960_720.jpg",
   }
-}
+]
+};
 
 export default App;
